01d0eef13c3e22d9ef6b2be5892581ae6d8d8189
irene macapinlac <irenekaymac@gmail.com>
2021-07-11 23:19:15 +1000

    game logic

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -4,7 +4,6 @@ user_wins = 0
 computer_wins = 0
 
 options = ["rock", "paper", "scissors"]
-# options[0, 1, 2]
 
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
@@ -16,5 +15,23 @@ while True:
 
         random_number = random.randint(0, 2)
         # rock: 0, paper: 1, scissors: 2
+        computer_pick = options[random_number]
+        print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors"
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "paper" and computer_pick == "rock"
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper"
+        print("You won!")
+        user_wins += 1
+
+    else:
+        print("You Lose!")
+        computer_wins += 1
 
 print("Thanks for playing")
